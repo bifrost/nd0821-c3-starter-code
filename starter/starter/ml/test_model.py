647034262abaa72b0709bd1e88a9e999621e049b
bifrost <dra@debitoor.com>
2023-03-07 12:10:07 +0100

    add 3 tests

---
+++ starter/starter/ml/test_model.py
@@ -0,0 +1,28 @@
+from .model import train_model, compute_model_metrics, inference
+from sklearn.ensemble import RandomForestClassifier
+
+def test_train_model():
+    X_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    y_train = [0, 1, 1, 0]
+    model = train_model(X_train, y_train)
+    assert type(model) == type(RandomForestClassifier())
+    assert model.predict([[0, 0]]) == [0]
+    assert model.predict([[0, 1]]) == [1]
+
+def test_compute_model_metrics():
+    y_pred = [0, 1, 1, 1]
+    y_train = [0, 1, 1, 0]
+
+    precision, recall, fbeta = compute_model_metrics(y_train, y_pred)
+    assert precision == 0.6666666666666666
+    assert recall == 1
+    assert fbeta == 0.8
+
+def test_inference():
+    X_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    y_train = [0, 1, 1, 0]
+    clf = RandomForestClassifier()
+    clf.fit(X_train, y_train)
+
+    result = inference(clf, [[0, 0], [0, 1]])
+    assert list(result) == [0, 1]
